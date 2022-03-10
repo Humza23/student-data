222cfcf061a82f9d415786beea854ab199ee3854
Humza23 <79542316+Humza23@users.noreply.github.com>
2022-03-10 14:16:54 -0500

    list of students + dynamic studentID routing

---
+++ src/components/ListOfStudents.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import studentData from '../data/students.json'
+import { Link } from 'react-router-dom'
+
+
+
+const ListOfStudents = () => {
+  return (
+    <div>
+      { studentData.map(student => (
+        <div className='studentName' key={student.StudentID} onClick={()=>console.log(student.StudentID)}>
+                <Link to={`/${student.StudentID}`}>
+
+            <p> {student.FirstName} {student.LastName} </p>
+
+            </Link>
+
+            </div>
+      ))
+      }
+</div>
+      );
+};
+
+export default ListOfStudents
