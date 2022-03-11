92285dceae721874d0ad72b1cf4dbb32e55026d7
Humza23 <79542316+Humza23@users.noreply.github.com>
2022-03-11 17:38:48 -0500

    alphabetical

--- src/components/ListOfStudents.js
+++ src/components/ListOfStudents.js
@@ -6,13 +6,14 @@ import { Link } from 'react-router-dom'
 
 const ListOfStudents = (props) => {
     const {studentData} = props
+    const sortedStudentData = studentData.sort((a, b) => a.FirstName.localeCompare(b.FirstName))
     
   return (
     <div className='studentList'>
       <h1 style={{textDecoration: 'underline'}}>
       NY01MS 07 - New York University Students
       </h1>
-      { studentData.map(student => (
+      { sortedStudentData.map(student => (
         <div className='studentName' key={student.StudentID} onClick={()=>console.log(student.StudentID)}>
                 <Link to={`/${student.StudentID}`}>
 
